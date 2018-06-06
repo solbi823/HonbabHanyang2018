5a333b477b8d60ac4d74964de29621239fda04ca
송성근 <el9371@gmail.com>
2018-06-07 02:02:48 +0900

    history setting

---
+++ xcode_proj/Honbabhanyang/HistoryDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HistoryDetailViewController.swift
+//  Honbabhanyang
+//
+//  Created by hodo on 2018. 6. 6..
+//  Copyright © 2018년 hojun. All rights reserved.
+//
+
+import UIKit
+
+class HistoryDetailViewController: UIViewController {
+
+    
+    
+    var partyDetail : Party?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func report(_ sender: Any) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
