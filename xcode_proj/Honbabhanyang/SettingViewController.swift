ee224f3ceb1044c41d75cb02c777631c7ac6c4af
송성근 <el9371@gmail.com>
2018-06-07 12:56:57 +0900

    history and setting

---
+++ xcode_proj/Honbabhanyang/SettingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SettingViewController.swift
+//  Honbabhanyang
+//
+//  Created by hodo on 2018. 6. 7..
+//  Copyright © 2018년 hojun. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func logOut(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
